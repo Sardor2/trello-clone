b67b3e462fa43418776cad203a41335db677c883
sardorX <abdurakhimov.sardor@gmail.com>
2022-03-10 12:51:00 +0500

    added server scripts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,8 @@
 import { Box, styled } from "@mui/material";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { Header } from "../commons/components/Header";
 import "../styles.css";
 import { ListContainer } from "../features";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import { AddListForm } from "features/lists/components/AddListForm";
 
 const BoxCanvas = styled(Box)`
@@ -19,12 +18,8 @@ const BoxCanvas = styled(Box)`
 `;
 
 export default function App() {
-  const scrollRef = useRef<HTMLElement>();
-  useEffect(() => {
-    scrollRef.current?.scrollTo(scrollRef.current.scrollWidth, 0);
-  }, []);
   return (
-    <BoxCanvas ref={scrollRef}>
+    <BoxCanvas>
       <Header />
       <Box display={"flex"} marginTop={15}>
         <ListContainer />
